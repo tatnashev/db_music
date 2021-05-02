e500c98accb097958788df313d3188cf1cf79429
rsaddatimov <rafa.saddatimov@gmail.com>
2021-05-03 02:39:47 +0300

    some refactoring

--- server/handlers/artists.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-
-
-def artists_list_handler():
-    return render_template('artists.html')
--- server/main.py
+++ server/main.py
@@ -3,16 +3,27 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-from . import handlers
-
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='dev-secret',
-    DATABASE=os.path.join(app.instance_path, 'db_saved.sql'),
     SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL'],
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+
+    genre_id = db.Column(db.Integer, primary_key=True)
+    genre_name = db.Column(db.String(30))
+
+
+class Country(db.Model):
+    __tablename__ = 'country'
+
+    country_id = db.Column(db.Integer, primary_key=True)
+    country_name = db.Column(db.String(50))
 
 
 # Main page handler
@@ -21,4 +32,16 @@ def hello():
     return render_template('index.html')
 
 
-app.add_url_rule('/artists', view_func=handlers.artists_list_handler)
+@app.route('/artists')
+def artists_list_handler():
+    return render_template('artists.html')
+
+
+@app.route('/genres')
+def genres_list_handler():
+    return render_template('genres.html')
+
+
+@app.route('/countries')
+def countries_list_handler():
+    return render_template('countries.html')
