906931f9b5cb802ab05f521616bb970163818f31
tatnashev <tatnashev@gmail.com>
2021-05-06 16:13:38 +0300

    db fix & content

--- tables_script.sql
+++ tables_script.sql
@@ -1,80 +1,74 @@
-CREATE TABLE genre (
-	genre_id SERIAL NOT NULL UNIQUE, 
-	genre_name char(30),
-	PRIMARY KEY (genre_id)
-); 
-
-CREATE TABLE country ( 
-	country_id SERIAL NOT NULL UNIQUE, 
-	country_name char(50), 
-	PRIMARY KEY (country_id)
-); 
-
-CREATE TABLE groups ( 
-	group_id SERIAL NOT NULL UNIQUE, 
-	artist_id SERIAL NOT NULL UNIQUE, 
-	group_name char(50), 
-	PRIMARY KEY (group_id)
-); 
-
 CREATE TABLE artist (
 	artist_id SERIAL NOT NULL UNIQUE, 
-	country_id SERIAL NOT NULL UNIQUE,
-	PRIMARY KEY (artist_id, country_id), 
-	FOREIGN KEY (country_id) REFERENCES country(country_id) ON DELETE CASCADE, 
-	FOREIGN KEY (artist_id) REFERENCES groups(group_id) ON DELETE CASCADE
-); 
+	artist_name char(50), 
+	bio char(1000),
+	PRIMARY KEY (artist_id)
+);
+
+
+CREATE TABLE genre (
+	genre_id SERIAL NOT NULL UNIQUE, 
+	genre_name char(30), 
+	PRIMARY KEY (genre_id)
+);
+
+
+CREATE TABLE country (
+	country_id SERIAL NOT NULL UNIQUE, 
+	country_name char(30), 
+	PRIMARY KEY (country_id)
+);
+
 
 CREATE TABLE genre_artist (
-	artist_id SERIAL NOT NULL, 
-	genre_id SERIAL NOT NULL, 
+	artist_id int NOT NULL, 
+	genre_id int NOT NULL, 
 	PRIMARY KEY (artist_id, genre_id), 
 	FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON UPDATE CASCADE, 
-	FOREIGN KEY (genre_id) REFERENCES genre(genre_id) ON UPDATE CASCADE
-); 
+	FOREIGN KEY (genre_id) REFERENCES genre(genre_id) ON UPDATE CASCADE	
+);
 
-CREATE TABLE personality (
-	artist_id SERIAL NOT NULL UNIQUE, 
+
+CREATE TABLE country_artist (
+	artist_id int NOT NULL, 
+	country_id int NOT NULL, 
+	PRIMARY KEY (artist_id, country_id), 
+	FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON UPDATE CASCADE, 
+	FOREIGN KEY (country_id) REFERENCES country(country_id) ON UPDATE CASCADE
+);
+
+
+CREATE TABLE musician (
+	musician_id SERIAL NOT NULL UNIQUE, 
+	artist_id int NOT NULL, 
 	last_name char(30), 
 	first_name char(30), 
-	birthday date, 
-	country char(30), 
-	short_bio char(150),
-	PRIMARY KEY (artist_id), 
-	CONSTRAINT fk_artist_id FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+	PRIMARY KEY (musician_id), 
+	FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE
 ); 
 
-CREATE TABLE group_history (
-	group_id SERIAL NOT NULL UNIQUE, 
-	team char(50), 
-	short_bio char(70), 
-	bio char(150),
-	PRIMARY KEY (group_id), 
-	CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups(group_id)
-); 
 
 CREATE TABLE album (
 	album_id SERIAL NOT NULL UNIQUE, 
-	artist_id SERIAL NOT NULL UNIQUE, 
-	album_year INTEGER, 
-	album_title char(50), 
-	album_tracks char(150), 
+	artist_id int NOT NULL, 
+	album_year int, 
+	album_title char(120), 
 	PRIMARY KEY (album_id), 
 	FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE
 );
 
-CREATE TABLE tracks (
-	track_id INTEGER NOT NULL, 
-	track_title char(50), 
-	track_duration float, 
-	PRIMARY KEY (track_id)
-);  
 
-CREATE TABLE album_tracks (
-	album_id SERIAL NOT NULL UNIQUE, 
-	track_id INTEGER NOT NULL, 
-	track_number integer,  
-	PRIMARY KEY (album_id),
-	FOREIGN KEY (track_id) REFERENCES tracks(track_id) ON DELETE CASCADE, 
-	FOREIGN KEY (album_id) REFERENCES album(album_id) ON DELETE CASCADE
+CREATE TABLE track (
+	track_id SERIAL NOT NULL UNIQUE, 
+	track_title char (120), 
+	PRIMARY KEY(track_id)
 ); 
+
+
+CREATE TABLE album_track (
+	album_id int NOT NULL, 
+	track_id int NOT NULL, 
+	PRIMARY KEY (album_id, track_id), 
+	FOREIGN KEY (track_id) REFERENCES track(track_id) ON UPDATE CASCADE, 
+	FOREIGN KEY (album_id) REFERENCES album(album_id) ON UPDATE CASCADE
+);
